ed837e58234c0847c9df471ffc935fa6372d2ded
l0n3l4t1n0 <le.trigue@gmail.com>
2024-03-21 21:00:15 -0600

    Other exception classes pushed

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/OutOfPaperException.java
@@ -0,0 +1,7 @@
+package com.thelocalmarketplace.software;
+
+public class OutOfPaperException throws Exception{
+    public OutOfPaperException(String paperErrorMessage) {
+        super(paperErrorMessage);
+        }
+}
