c873a21542518c8e71c08d5c3fbbbbcbe9fb37f1
taragmrad <147683744+taragmrad@users.noreply.github.com>
2024-03-21 18:48:39 -0600

    Create mockScale.java

---
+++ com.thelocalmarketplace.software.test/src/mockScale.java
@@ -0,0 +1,85 @@
+/**
+ * Yotam Rojnov (UCID: 30173949)
+ * Duncan McKay (UCID: 30177857)
+ * Mahfuz Alam (UCID:30142265)
+ * Luis Trigueros Granillo (UCID: 30167989)
+ * Lilia Skumatova (UCID: 30187339)
+ * Abdelrahman Abbas (UCID: 30110374)
+ * Talaal Irtija (UCID: 30169780)
+ * Alejandro Cardona (UCID: 30178941)
+ * Alexandre Duteau (UCID: 30192082)
+ * Grace Johnson (UCID: 30149693)
+ * Abil Momin (UCID: 30154771)
+ * Tara Ghasemi M. Rad (UCID: 30171212)
+ * Izabella Mawani (UCID: 30179738)
+ * Binish Khalid (UCID: 30061367)
+ * Fatima Khalid (UCID: 30140757)
+ * Lucas Kasdorf (UCID: 30173922)
+ * Emily Garcia-Volk (UCID: 30140791)
+ * Yuinikoru Futamata (UCID: 30173228)
+ * Joseph Tandyo (UCID: 30182561)
+ * Syed Haider (UCID: 30143096)
+ * Nami Marwah (UCID: 30178528)
+ */
+
+package com.thelocalmarketplace.software.test;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.jjjwelectronics.Item;
+import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.OverloadedDevice;
+import com.jjjwelectronics.Mass.MassDifference;
+import com.jjjwelectronics.scale.ElectronicScale;
+import com.jjjwelectronics.scale.ElectronicScaleListener;
+
+import ca.ucalgary.seng300.simulation.InvalidArgumentSimulationException;
+import ca.ucalgary.seng300.simulation.NullPointerSimulationException;
+import powerutility.NoPowerException;
+
+public class mockScale extends ElectronicScale {
+	private List<Item> items = new ArrayList<>();
+    private Mass currentMass = Mass.ZERO;
+    private Mass massLimit;
+    private Mass sensitivityLimit;
+
+
+
+    @Override
+    public Mass getMassLimit() {
+        return massLimit;
+    }
+
+    @Override
+    public Mass getSensitivityLimit() {
+        return sensitivityLimit;
+    }
+
+    @Override
+    public synchronized void addAnItem(Item item) {
+        currentMass = currentMass.sum(item.getMass());
+        items.add(item);
+        
+    }
+
+    @Override
+    public synchronized void removeAnItem(Item item) {
+        items.remove(item);
+        currentMass = calculateCurrentMass();
+    }
+
+    private Mass calculateCurrentMass() {
+        Mass newMass = Mass.ZERO;
+        for (Item item : items) {
+            newMass = newMass.sum(item.getMass());
+        }
+        return newMass;
+    }
+    @Override
+    public Mass getCurrentMassOnTheScale() {
+        return currentMass;
+    }
+}
