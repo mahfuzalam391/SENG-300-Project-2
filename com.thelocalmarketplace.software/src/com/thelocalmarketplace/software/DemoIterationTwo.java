6ddd40336ea8c33e50b094b63c816d4dbedd974e
yotam1802 <Yotam1802@gmail.com>
2024-03-24 15:32:57 -0600

    Turned off the selfcheckout station

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/DemoIterationTwo.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/DemoIterationTwo.java
@@ -278,7 +278,7 @@ public class DemoIterationTwo {
             }
             // Proper closure of resources and final messages can be added here
             System.out.println("Thank you for using the self-checkout system.");
-            return;
+            System.exit(0);
 
         } catch (Exception e) {
             System.out.println("Failed to initialize order: " + e.getMessage());
