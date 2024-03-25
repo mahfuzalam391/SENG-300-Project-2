f96c4d4979f8dd5c18bae1f62fcad840ec25718d
yuinikoru.futamata <yuinikoru.futamata@ucalgary.ca>
2024-03-24 23:17:39 -0600

    Fixed a comment

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Order.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Order.java
@@ -173,10 +173,10 @@ public class Order {
 
 	/**
 	 * Checks for weight discrepancy, is called by the baggingAreaListener after an item is added to the bagging area scale
+	 * and the signalToRemoveItemFromOrder after the item is removed from the order.
 	 * @throws OverloadedDevice
 	 */
 	public void checkForDiscrepancy() throws OverloadedDevice {
-		// This method is called by the baggingAreaListener after an item is added to the bagging area scale
 		WeightDiscrepancy weightDiscrepancy = new WeightDiscrepancy(this, scale);
 
 		weightDiscrepancy.checkIfCanUnblock(); // Checks for a weight discrepancy, if none, it unblocks the system
